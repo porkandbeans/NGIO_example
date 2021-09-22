f9a4e89ef31a60b454001c3645f96b410e0c35f6
Charlie <voter96@gmail.com>
2021-09-22 22:07:12 +1200

    worked a bit more on this thing

--- Api_logic.cs
+++ Api_logic.cs
@@ -1,33 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Api_logic : MonoBehaviour
 {
     private io.newgrounds.core ngio_core;
+    public Text usernameText;
 
     // Gets called when the player is signed in.
     void onLoggedIn() 
     {
-        // Do something. You can access the player's info with:
         io.newgrounds.objects.user player = ngio_core.current_user;
+        //username as a string can be accessed with
+        string username = player.name;
+        usernameText.text = username; 
+
+        // Since this function is called when the user is DEFINITELY logged in, it's a good idea to call stuff related to
+        // the API here. For instance, unlocking medals, submitting high-scores, switching to the next scene if you want to do that...
+        SceneManager.LoadScene("Scene 2");
     }
 
     // Gets called if there was a problem with the login (expired sessions, server problems, etc).
     void onLoginFailed() 
     {
-        // Do something. You can access the login error with:
         io.newgrounds.objects.error error = ngio_core.login_error;
-    }
-
-    // Gets called if the user cancels a login attempt.
-    void onLoginCancelled() 
-    {
-        // Do something...
+        Debug.Log(error); // maybe you wanna see why it failed?
+        usernameText.text = "Failed to log in!";
     }
 
     // When the user clicks your log-in button
-    void requestLogin() 
+    public void requestLogin() 
     {
         // This opens passport and tells the core what to do when a definitive result comes back.
         ngio_core.requestLogin(onLoggedIn, onLoginFailed, onLoginCancelled);
@@ -49,6 +53,7 @@ public class Api_logic : MonoBehaviour
     // Check if the user has a saved login when your game starts
     void Start() 
     {
+        
         ngio_core = GetComponent<io.newgrounds.core>();
         // Do this after the core has been initialized
         ngio_core.onReady(() => { 
@@ -60,10 +65,6 @@ public class Api_logic : MonoBehaviour
                 {
                     onLoggedIn();
                 }
-                else 
-                {
-                    requestLogin();
-                }
             });
         });
     }
@@ -73,4 +74,6 @@ public class Api_logic : MonoBehaviour
     {
         ngio_core.logOut();
     }
+
+    void onLoginCancelled() => Debug.Log("Login was cancelled...");
 }
